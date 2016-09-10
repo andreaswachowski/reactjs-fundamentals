1dabfb6d671715971bada463d6f600121660a275
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-10 08:58:12 +0200

    feat: add avatar components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,57 @@
+var USER_DATA = {
+  name: 'Andreas Wachowski',
+  username: 'andreaswachowski',
+  image: 'http://www.andreas-wachowski.de/images/profile_picture.170254e3.png'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
 
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div> Hello World </div>
+      <div> Hello {this.props.name} </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
