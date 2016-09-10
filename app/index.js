4d3fa54e5037479123953a41dffeffc8e70dce84
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-10 09:20:18 +0200

    feat: switch to react-router

--- app/index.js
+++ app/index.js
@@ -1,73 +1,8 @@
-var USER_DATA = {
-  name: 'Andreas Wachowski',
-  username: 'andreaswachowski',
-  image: 'http://www.andreas-wachowski.de/images/profile_picture.170254e3.png'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
-
-var Link = React.createClass({
-  changeURL: function() {
-    window.location.replace(this.props.href)
-  },
-
-  render: function() {
-    return (
-      <span
-        style={{color: 'blue', cursor: 'pointer'}}
-        onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    )
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Link href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return <div>{this.props.name}</div>
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
-
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div> Hello {this.props.name} </div>
-    )
-  }
-});
+var routes = require('./config/routes')
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
