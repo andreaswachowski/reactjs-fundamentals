2f3725c3ae3a3f549d4133988cff2f0d4e0a8274
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-12 11:00:28 +0200

    feat(ConfirmBattle): add basic battle confirmation

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,6 +5,7 @@ var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
@@ -15,6 +16,7 @@ var routes = (
       <IndexRoute component={Home} />
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
     </Route>
   </Router>
 );
