d17146bd14f1551298e1c7d42f30c56863c07c19
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-27 15:35:08 +0200

    refa(Loading): use concise object methods

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -22,19 +22,22 @@ var Loading = React.createClass({
     text: PropTypes.string,
     speed: PropTypes.number
   },
-  getDefaultProps: function() {
+
+  getDefaultProps() {
     return {
       text: 'Loading',
       speed: 300
     };
   },
-  getInitialState: function() {
+
+  getInitialState() {
     this.originalText = this.props.text;
     return {
       text: this.originalText
     };
   },
-  componentDidMount: function() {
+
+  componentDidMount() {
     var stopper = this.originalText + '...';
     this.interval = setInterval(function() {
       if (this.state.text === stopper) {
@@ -48,10 +51,12 @@ var Loading = React.createClass({
       }
     }.bind(this), this.props.speed);
   },
-  componentWillUnmount: function() {
+
+  componentWillUnmount() {
     clearInterval(this.interval);
   },
-  render: function() {
+
+  render() {
     return <div style={styles.container}>
       <p style={styles.content}>{this.state.text}</p>
     </div>;
