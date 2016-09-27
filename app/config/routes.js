6d658de8e5ec87b454462458a8c34b94f5ada5c7
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-27 10:56:20 +0200

    refa(Home): use ES6 imports

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,7 +9,7 @@ var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 var ResultsContainer = require('../containers/ResultsContainer');
 
 var Main = require('../components/Main');
-var Home = require('../components/Home');
+import Home from '../components/Home';
 
 var routes =
   <Router history={hashHistory}>
