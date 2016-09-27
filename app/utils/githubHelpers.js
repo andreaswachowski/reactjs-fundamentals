a951208b4c77f551c875334dbd96d4a194a6c0cc
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-27 15:50:44 +0200

    refa(githubHelpers): use arrow functions

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -14,20 +14,18 @@ function getRepos(username) {
 }
 
 function getTotalStars(repos) {
-  return repos.data.reduce(function(prev, current) {
-    return prev + current.stargazers_count;
-  }, 0);
+  return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0);
 }
 
 function getPlayersData(player) {
   return getRepos(player.login)
     .then(getTotalStars)
-    .then(function(totalStars) {
-      return {
+    .then(totalStars => (
+      {
         followers: player.followers,
         totalStars: totalStars
-      };
-    });
+      }
+    ));
 }
 
 function calculateScores(players) {
@@ -38,18 +36,14 @@ function calculateScores(players) {
 }
 
 export function getPlayersInfo(players) {
-  return axios.all(players.map(function(username) {
-    return getUserInfo(username);
-  })).then(function(info) {
-    return info.map(function(user) {
-      return user.data;
+  return axios.all(players.map(username => getUserInfo(username)))
+    .then(info => info.map(user => user.data))
+    .catch(function(error) {
+      return logCustomMessage(error.message, {
+        players: players,
+        error: error
+      });
     });
-  }).catch(function(error) {
-    return logCustomMessage(error.message, {
-      players: players,
-      error: error
-    });
-  });
 }
 
 export function battle(players) {
