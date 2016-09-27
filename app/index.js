8010f297fc8fdca8336f2b0e8ee4b8ac7e2fbe96
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-27 15:05:08 +0200

    refa(app/index): use ES6 imports

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-var ReactDOM = require('react-dom');
+import ReactDOM from 'react-dom';
 import routes from './config/routes';
-var Raven = require('raven-js');
+import Raven from 'raven-js';
 
 var sentryKey = '077f18e15f3f4ae896b980a810f8b5a4';
 var sentryApp = '98119';
