3b469422d0024c970a510db2583fb24b78057224
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-28 17:33:46 +0200

    refa(PromptContainer): refactor to ES6 classes

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Prompt from '../components/Prompt';
 
-var PromptContainer = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object.isRequired
-  },
-  getInitialState: function() {
-    return {
+class PromptContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
       username: ''
     };
-  },
-  handleUpdateUser: function(e) {
+  }
+
+  handleUpdateUser(e) {
     this.setState({
       username: e.target.value
     });
-  },
-  handleSubmitUser: function(e) {
+  }
+
+  handleSubmitUser(e) {
     e.preventDefault();
     const {username} = this.state;
 
@@ -36,17 +36,22 @@ var PromptContainer = React.createClass({
     } else {
       this.context.router.push(`/playerTwo/${username}`);
     }
-  },
-  render: function() {
+  }
+
+  render() {
     return (
       <Prompt
-        onSubmitUser={this.handleSubmitUser}
-        onUpdateUser={this.handleUpdateUser}
+        onSubmitUser={(event) => this.handleSubmitUser(event)}
+        onUpdateUser={(event) => this.handleUpdateUser(event)}
         header={this.props.route.header}
         username={this.state.username}
       />
     );
   }
-});
+}
+
+PromptContainer.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default PromptContainer;
