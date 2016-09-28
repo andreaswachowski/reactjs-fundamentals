55ea0ce54371a18a851dd152fc94f3fb1b62b3f7
Andreas Wachowski <andreas.wachowski@akra.de>
2016-09-28 16:41:20 +0200

    refa(githubHelpers): use async/await

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -17,15 +17,17 @@ function getTotalStars(repos) {
   return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0);
 }
 
-function getPlayersData(player) {
-  return getRepos(player.login)
-    .then(getTotalStars)
-    .then(totalStars => (
-      {
-        followers: player.followers,
-        totalStars
-      }
-    ));
+async function getPlayersData(player) {
+  try {
+    const repos = await getRepos(player.login);
+    const totalStars = await getTotalStars(repos);
+    return {
+      followers: player.followers,
+      totalStars
+    }
+  } catch(error) {
+    console.warn('Error in getPlayersData: ', error);
+  }
 }
 
 function calculateScores(players) {
@@ -35,17 +37,25 @@ function calculateScores(players) {
   ];
 }
 
-export function getPlayersInfo(players) {
-  return axios.all(players.map(username => getUserInfo(username)))
-    .then(info => info.map(user => user.data))
-    .catch(error => logCustomMessage(error.message, {players, error}));
+export async function getPlayersInfo(players) {
+  try {
+    const info = await Promise.all(players.map(username => getUserInfo(username)));
+    return info.map(user => user.data);
+  }
+  catch (error) {
+    logCustomMessage(error.message, {players, error});
+  }
 }
 
-export function battle(players) {
+export async function battle(players) {
   const playerOneData = getPlayersData(players[0]);
   const playerTwoData = getPlayersData(players[1]);
 
-  return axios.all([playerOneData, playerTwoData])
-    .then(calculateScores)
-    .catch(error => logCustomMessage(error.message, {players, error}));
+  try {
+    const playerData = await Promise.all([playerOneData, playerTwoData])
+    return calculateScores(playerData);
+  }
+  catch (error) {
+    logCustomMessage(error.message, {players, error});
+  }
 }
